3d1d519ff921e73ae5577cf8d02da9902e841744
imanj12 <imanj12@gmail.com>
2018-11-13 14:27:21 -0500

    added edit user ability

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,19 @@ class UsersController < ApplicationController
     @recents = @user.recent_five_posts
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
