dcc355c1bfc30b73a199f8abedff4629ea4dd546
imanj12 <imanj12@gmail.com>
2018-11-14 09:29:09 -0500

    tried some styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :discussions, through: :posts
-
   validates :username, uniqueness: true
+  has_secure_password
 
   def recent_five_posts
     self.posts.last(5).reverse
