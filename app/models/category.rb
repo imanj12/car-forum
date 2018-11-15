78d6c2df7cce111a655bd1385329a7a96d534439
imanj12 <imanj12@gmail.com>
2018-11-15 14:43:26 -0500

    added style to cat page and a little to disc page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,20 @@
 class Category < ApplicationRecord
   has_many :discussions
 
-  # total number of discussions
   def total_threads
     self.discussions.count
   end
+
+  def total_posts
+    total_posts = []
+    self.discussions.each do |disc|
+      total_posts << disc.posts.length
+    end
+    total_posts.inject(:+)
+  end
+
+  # def last_post
+  #   self.discussions.last
+  # end
+
 end
