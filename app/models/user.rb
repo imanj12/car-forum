8d8d4a34f576469121903d99e5033e8e3caf690d
imanj12 <imanj12@gmail.com>
2018-11-16 10:15:43 -0500

    added catchall route at end of routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :discussions, through: :posts
   validates :username, uniqueness: true
   validates :username, presence: true
+  validates :password, length: { minimum: 8 }, allow_nil: false
   has_secure_password
 
   def recent_five_posts
--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '*path', to: 'categories#index'
 end
